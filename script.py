e6b663ccf8447fb6e8ded5a0bce986d8aa704319
Daniel Barros <barros.daniel@outlook.com>
2026-04-24 12:08:09 +0800

    feat: Add days_until_delivery function
    Signed-off-by: Daniel Barros <barros.daniel@outlook.com>

--- script.py
+++ script.py
@@ -35,3 +35,22 @@ def calculate_landing_time(rocket_launch_dt: datetime, travel_duration: int) -> 
     days: timedelta = timedelta(days=travel_duration)
     arrival: datetime = rocket_launch_dt + days
     return arrival.strftime("%d-%m-%Y")
+
+
+def days_until_delivery(expected_delivery_dt: datetime, current_dt: datetime):
+    """Calculates the number of days until the expected delivery date.
+
+    Args:
+        expected_delivery_dt (datetime): The expected delivery date.
+        current_dt (datetime): The current date.
+
+    Returns:
+        int: The number of days until delivery. If the expected delivery date has already passed,
+            this function will return a negative number.
+
+    Example:
+        >>> days_until_delivery(datetime(2023, 2, 15), datetime(2023, 2, 5))
+        10
+    """
+    time_remaining: timedelta = expected_delivery_dt - current_dt
+    return time_remaining.days
--- test_script.py
+++ test_script.py
@@ -1,5 +1,9 @@
 from datetime import datetime
-from script import calculate_landing_time, format_date
+from script import (
+    calculate_landing_time,
+    days_until_delivery,
+    format_date
+)
 
 
 def test_format_date():
@@ -8,3 +12,8 @@ def test_format_date():
 
 def test_calculate_landing_time():
     assert calculate_landing_time(datetime(2023, 2, 15), 20) == '07-03-2023'
+
+
+def test_days_until_delivery():
+    assert days_until_delivery(
+        datetime(2023, 2, 15), datetime(2023, 2, 5)) == 10
